7953cc79e1700b2360305ba260bccfeeac0e498e
georgii <georgii.krikun@gmail.com>
2021-11-01 21:08:39 +0100

    Goglike logging

---
+++ Source/Warlocks/GoglikeLogging.h
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(Goglike, Log, All);
+#define GW(X, ...) UE_LOG(Goglike, Warning, TEXT(X), __VA_ARGS__);
+#define GL(X, ...) UE_LOG(Goglike, Log, TEXT(X), __VA_ARGS__);
+#define GF(X, ...) UE_LOG(Goglike, Fatal, TEXT(X), __VA_ARGS__);
+#define GE(X, ...) UE_LOG(Goglike, Error, TEXT(X), __VA_ARGS__);
