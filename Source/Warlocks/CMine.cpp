5e9a51ae7da1eba1e5846ec656506b4206ab1996
georgii <georgii.krikun@gmail.com>
2021-11-08 21:48:56 +0100

    Update for the source compiled UE4

--- Source/Warlocks/CMine.cpp
+++ Source/Warlocks/CMine.cpp
@@ -22,7 +22,7 @@ void UCMine::SpawnMine_Implementation(FVector location, FVector direction)
 	auto warlock = Cast<ACWarlockChar>(GetOwner());
 	if (!warlock) return;
 	FVector warlockLocation = warlock->GetActorLocation();
-	direction = (location - warlockLocation).GetSafeNormal2D
+	direction = (location - warlockLocation).GetSafeNormal2D();
 	FVector spawnLocation = warlockLocation + 100.0f * direction;
 	FRotator spawnRotation = direction.ToOrientationRotator();
 
